295dab4d91513eb3c01441b2a11763c716e5023d
Oscar Ferraro <oscar@oferraro.oferraro.local>
2018-07-14 03:01:08 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Just a repo with shell scripts
+Download the files
+Add permissions, chmod u+x *.sh
---
+++ checkssl.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+MSG=""
+if [ -f doms ]; then
+    DOMS=$(cat doms)
+else
+    echo "file doms doesn't exist, create a file named doms with domain names, 1 domain per line"
+    echo "i.e: file doms: "
+    echo "dom1.com"
+    echo "dom2.com"
+    echo "dometc.com"
+    exit 1
+fi
+
+for DOM in $DOMS; do 
+    echo "checking $DOM"
+    EXPDATE=`date --date="$(echo | openssl s_client  -connect $DOM:443 2>/dev/null | openssl x509 -noout -issuer -subject -dates | grep -i notbefore | cut -d = -f 2)" --iso-8601`
+    CURDATE=`date --iso-8601`
+
+    DATEDIFF="$(( ($(date -d "$EXPDATE" '+%s') - $(date -d "$CURDATE" '+%s'))/60/60/24))"
+
+    DIFFLIMIT=15
+
+    if [ $DATEDIFF -lt $DIFFLIMIT ]; then 
+        MSG="$MSG for $DOM diff: $DATEDIFF is lower than $DIFFLIMIT  days \n"
+        MSG="$MSG curdate:  $CURDATE \n"
+        MSG="$MSG EXPDATE:  $EXPDATE \n"
+        MSG="$MSG ---------------------------------------- \n \n"
+    fi
+    echo "checked $DOM"
+done
+
+if [ ! -z "$MSG" ]; then
+    zenity --error --text="$MSG"
+else
+    echo "all sites expires after more than $DIFFLIMIT days"
+fi 
+
+echo "certbot renew will fix it on server"
